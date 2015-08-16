b8ab9c13c045e74b9354c180b4e4dad195614cca
antonija simic <antonija1023@gmail.com>
2015-08-16 17:23:00 +0200

    fixing typo

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessor :remeber_token
+  attr_accessor :remember_token
   before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -21,8 +21,13 @@ class User < ActiveRecord::Base
     SecureRandom.urlsafe_base64
   end
 
-  def remember
+   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+   # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 end
