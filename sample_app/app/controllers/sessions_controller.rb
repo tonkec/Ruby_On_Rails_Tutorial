67213cad0481dd3e230db49d13252f5cbd387678
antonija simic <antonija1023@gmail.com>
2015-08-16 22:30:00 +0200

    improving method for loggin in the user with new code for checking the status of checkbox

--- sample_app/app/controllers/sessions_controller.rb
+++ sample_app/app/controllers/sessions_controller.rb
@@ -6,7 +6,12 @@ class SessionsController < ApplicationController
   	user = User.find_by(email: params[:session][:email].downcase)
   	if user && user.authenticate(params[:session][:password])
   	  log_in user
-      remember user
+      #if checkbox is checked it is 1
+      if params[:session][:remember_me] == '1'
+        remember(user)
+      else
+        forget(user)
+      end
       redirect_to user
     else
       flash.now[:danger] = "Invalid email/password combination"
