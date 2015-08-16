1d7cdd51114a99e1ce4511206d0f3d5375ade507
antonija simic <antonija1023@gmail.com>
2015-08-16 17:25:07 +0200

    add method for remembering user and change current_user method because of permanent cookies implementation

--- sample_app/app/helpers/sessions_helper.rb
+++ sample_app/app/helpers/sessions_helper.rb
@@ -1,13 +1,29 @@
 module SessionsHelper
 	def log_in(user)
-		session[:user_id] = user.id 
+	  session[:user_id] = user.id 
+	end
+
+	# Remembers a user in a persistent session.
+	def remember(user)
+	  user.remember
+	  cookies.permanent.signed[:user_id] = user.id
+	  cookies.permanent[:remember_token] = user.remember_token
 	end
 
 	def current_user
-		if @current_user.nil?
-			@current_user = User.find_by(id: session[:user_id])
-		else
-			@current_user
+		if session[:user_id]
+			if @current_user.nil?
+				@current_user = User.find_by(id: session[:user_id])
+			else
+				@current_user
+			end
+		elsif cookies.signed[:user_id]
+			user = User.find_by(id: cookies.signed[:user_id])
+			if user && user.authenticate?(cookies[:remember_token])
+				log_in user
+				@current_user = user
+			end
+			
 		end
 	end
 
