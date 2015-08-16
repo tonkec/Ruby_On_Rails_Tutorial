474785951578234af7c063fc3dc4485360437aaa
antonija simic <antonija1023@gmail.com>
2015-08-16 17:07:03 +0200

    add method for rememberin user

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-
+  attr_accessor :remeber_token
   before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -19,5 +19,10 @@ class User < ActiveRecord::Base
   #generates random token
   def User.new_token
     SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 end
