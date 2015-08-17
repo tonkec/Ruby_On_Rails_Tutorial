517759867b2f4f68e7bc785124156f651525fcfb
antonija simic <antonija1023@gmail.com>
2015-08-17 13:27:30 +0200

    add flash mssg and redirect to profile page after sucessful edit

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -28,7 +28,8 @@ class UsersController < ApplicationController
   def update 
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      
+      flash[:success] = "Saved!"
+      redirect_to @user
     else
       render 'edit'
     end
