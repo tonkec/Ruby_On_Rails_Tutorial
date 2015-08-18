cdfc7a172167f9fa01b072a6381b06168fc19aff
antonija simic <antonija1023@gmail.com>
2015-08-18 18:51:38 +0200

    redirect to login_url not root_url (I made a mistake)

--- sample_app/test/controllers/users_controller_test.rb
+++ sample_app/test/controllers/users_controller_test.rb
@@ -46,7 +46,7 @@ class UsersControllerTest < ActionController::TestCase
     assert_no_difference 'User.count' do
       delete :destroy, id: @user
     end
-    assert_redirected_to root_url
+    assert_redirected_to login_url
   end
 
   test "should redirect destroy for non-admin" do 
