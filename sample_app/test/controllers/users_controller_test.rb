c4b2f12cc31cb1f46f7d5a2ef205c535e89fd9c1
antonija simic <antonija1023@gmail.com>
2015-08-18 12:33:44 +0200

    testing update and edit of one user when logged in as other user

--- sample_app/test/controllers/users_controller_test.rb
+++ sample_app/test/controllers/users_controller_test.rb
@@ -23,17 +23,17 @@ class UsersControllerTest < ActionController::TestCase
   	assert_redirected_to login_url
   end
 
-  test "should redirect edit when logged in as other user" do
-  	log_in_as(@other_user)
-  	get :edit, id: @user
-  	assert flash.empty?
-  	assert_redirected_to root_url
+  test "should redirect edit when logged in as wrong user" do
+    log_in_as(@other_user)
+    get :edit, id: @user
+    assert flash.empty?
+    assert_redirected_to root_url
   end
 
-  test "should redirect update when logged in as other user" do
-  	log_in_as(@other_user)
-  	get :update, id: @user, user: { name: @user.name, email: @user.email }
-  	assert flash.empty?
-  	assert_redirected_to root_url
+  test "should redirect update when logged in as wrong user" do
+    log_in_as(@other_user)
+    patch :update, id: @user, user: { name: @user.name, email: @user.email }
+    assert flash.empty?
+    assert_redirected_to root_url
   end
 end
