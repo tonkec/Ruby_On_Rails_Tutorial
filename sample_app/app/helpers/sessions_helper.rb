d86bdf29aa185c2674bd016dd6a4d26602c90711
antonija simic <antonija1023@gmail.com>
2015-08-18 12:28:03 +0200

    define methods for friendly forwarding, that is, not logged in users after logging in should be redirected to page they wanted to visit in the first place

--- sample_app/app/helpers/sessions_helper.rb
+++ sample_app/app/helpers/sessions_helper.rb
@@ -44,6 +44,15 @@ module SessionsHelper
 	end
 
 	def current_user?(user)
-	  user == current_user
+    user == current_user
+  end
+
+	def redirect_back_or(default)
+		redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+	end
+
+	def store_location
+		session[:forwarding_url] = request.url if request.get?
 	end
 end
