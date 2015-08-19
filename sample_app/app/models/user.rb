2efc2e8aca210f883057d090f7f1261694797122
antonija simic <antonija1023@gmail.com>
2015-08-19 09:40:01 +0200

    create method for account activation

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,14 +1,23 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token
-  before_save { self.email = email.downcase }
+  attr_accessor :remember_token, :activation_token
+  before_create :create_ativation_digest
+  before_save :downcase_email
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-   has_secure_password
-   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
+  def create_ativation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
+
+  def downcase_email 
+    self.email = email.downcase
+  end
    # Returns the hash digest of the given string.
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
