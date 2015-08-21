3966c79587c0ee5696ef65c1324271795d780521
antonija simic <antonija1023@gmail.com>
2015-08-21 20:37:57 +0200

    generating Microposts controller

--- sample_app/test/models/micropost_test.rb
+++ sample_app/test/models/micropost_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class MicropostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:antonija)
+    @micropost = @user.microposts.build(content: "Lorem Ipsum")
+  end
+
+  test "should be valid" do
+    assert @micropost.valid?
+  end
+
+  test "user id should be present" do
+    @micropost.user_id = nil
+    assert_not @micropost.valid? 
+  end
+
+  test "content should be present" do
+    @micropost.content = ""
+    assert_not @micropost.valid?
+  end
+
+  test "content should not be longer than 140 chr" do
+    @micropost.content = "" * 141
+    assert_not @micropost.valid?
+  end
+
+  test "order should be most recent first" do
+    assert_equal microposts(:most_recent), Micropost.first
+  end
+
+  
 end
