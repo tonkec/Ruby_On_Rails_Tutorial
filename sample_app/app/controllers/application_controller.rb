8a2eaaa7dc8c9d6288d3f74ca3e46af2aa16b726
antonija simic <antonija1023@gmail.com>
2015-08-22 12:46:43 +0200

    make private logged_in_user

--- sample_app/app/controllers/application_controller.rb
+++ sample_app/app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
+  private 
+  
   def logged_in_user
     unless logged_in?
     store_location
