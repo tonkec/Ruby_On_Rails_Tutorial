1c2c89f479700210b5842a2ffc6d1245abddeb6f
antonija simic <antonija1023@gmail.com>
2015-08-24 16:25:19 +0200

    add member following and followers to users

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -7,8 +7,12 @@ Rails.application.routes.draw do
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
-  resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 end
